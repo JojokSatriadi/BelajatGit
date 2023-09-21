6ccd643f418f916922166d323269f4808de40713
Jsatriadi <jojok.satriadi@gmail.com>
2023-09-21 14:21:27 +0700

    perubahan commit kedua

--- fungsi.js
+++ fungsi.js
@@ -1,4 +1,8 @@
 console.log("fungsi pertama");
 console.log("fungsi kedua");
 console.log("funsi ketiga");
+console.log("fungsi keempat");
+console.log("fungsi kelima");
+const add=(a,b) =>a+b ;
+console.log (add(1,2));
 
